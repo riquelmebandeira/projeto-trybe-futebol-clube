4e366ac5dcae22732b33f69ff3b41381d8f55d20
Riquelme <riquelmebandeira1@outlook.com>
2022-03-17 16:16:30 -0300

    Corrige tipagem de colunas na migration 'create-table-matchs'

--- app/backend/src/database/migrations/20220317185458-create-table-matchs.js
+++ app/backend/src/database/migrations/20220317185458-create-table-matchs.js
@@ -11,7 +11,7 @@ module.exports = {
       },
       home_team: {
         allowNull: false,
-        type: Sequelize.STRING,
+        type: Sequelize.INTEGER,
         references: {
           model: 'clubs',
           key: 'id'
@@ -23,7 +23,7 @@ module.exports = {
       },
       away_team: {
         allowNull: false,
-        type: Sequelize.STRING,
+        type: Sequelize.INTEGER,
         references: {
           model: 'clubs',
           key: 'id'
