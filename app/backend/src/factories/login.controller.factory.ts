1c5c58c3da2155089cca85072f3edc11f0ab198e
Riquelme <riquelmebandeira1@outlook.com>
2022-03-25 15:39:09 -0300

    Requisito 5 - cria uma Factory para o controller de login

---
+++ app/backend/src/factories/login.controller.factory.ts
@@ -0,0 +1,10 @@
+import LoginController from '../controllers';
+import LoginService from '../services';
+
+const loginControllerFactory = () => {
+  const loginService = new LoginService();
+  const loginController = new LoginController(loginService);
+  return loginController;
+};
+
+export default loginControllerFactory;
