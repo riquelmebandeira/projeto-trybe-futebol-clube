8830039d9bf121b0afcb7187e881b7906fdd04d0
Riquelme <riquelmebandeira1@outlook.com>
2022-03-25 16:05:57 -0300

    Requisito 5 - Router: implementa a rota POST /login

---
+++ app/backend/src/factories/index.ts
@@ -0,0 +1,3 @@
+import loginControllerFactory from './login.controller.factory';
+
+export default loginControllerFactory;
---
+++ app/backend/src/routes/login.router.ts
@@ -0,0 +1,23 @@
+import { Request, Response, Router } from 'express';
+import * as fs from 'fs/promises';
+import * as jwt from 'jsonwebtoken';
+import loginControllerFactory from '../factories';
+
+const loginRouter = Router();
+const loginController = loginControllerFactory();
+
+loginRouter.post('/', async (req: Request, res: Response) => {
+  const { email } = req.body;
+
+  const user = await loginController.login(email);
+
+  const { password, ...userInfo } = user;
+
+  const jwtSecret = await fs.readFile('../../jwt.evalution.key', 'utf-8');
+
+  const token = jwt.sign(userInfo, jwtSecret);
+
+  res.status(200).json({ user: userInfo, token });
+});
+
+export default loginRouter;
