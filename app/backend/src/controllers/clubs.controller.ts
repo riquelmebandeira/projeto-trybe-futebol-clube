8c5760e65f1474250b97ac60e12b6cab6496617e
Riquelme <riquelmebandeira1@outlook.com>
2022-04-07 09:13:34 -0300

    Requisito 17 - (Controller) implementa a rota GET /clubs/:id

--- app/backend/src/controllers/clubs.controller.ts
+++ app/backend/src/controllers/clubs.controller.ts
@@ -9,6 +9,12 @@ class ClubsController {
 
     return result as unknown as Club;
   }
+
+  async getById(id: number): Promise<Club> {
+    const result = await this.clubsService.getById(id);
+
+    return result as Club || null;
+  }
 }
 
 export default ClubsController;
