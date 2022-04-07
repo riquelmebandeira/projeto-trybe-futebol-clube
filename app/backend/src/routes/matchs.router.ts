0ab9e35bb6bdb76a6d4002080f01658393319ca2
Riquelme <riquelmebandeira1@outlook.com>
2022-04-07 11:57:31 -0300

    Requisitos 20 e 21 - (Router) implementa um filtro por query string na rota GET /matchs

--- app/backend/src/routes/matchs.router.ts
+++ app/backend/src/routes/matchs.router.ts
@@ -4,7 +4,17 @@ import { matchsControllerFactory } from '../factories';
 const matchsRouter = Router();
 const matchsController = matchsControllerFactory();
 
-matchsRouter.get('/', async (_req: Request, res: Response) => {
+matchsRouter.get('/', async (req: Request, res: Response) => {
+  const { inProgress } = req.query; // recebe uma STRING "true" ou "false"
+
+  if (inProgress) {
+    const booleanValue = (inProgress === 'true');
+
+    const matchs = await matchsController.getByProgress(booleanValue);
+
+    return res.status(200).json(matchs);
+  }
+
   const matchs = await matchsController.getAll();
 
   res.status(200).json(matchs);
