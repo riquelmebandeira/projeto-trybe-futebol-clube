457068a1f45bb6681b9ae9b26a3a29048a76f06d
Riquelme <riquelmebandeira1@outlook.com>
2022-04-09 01:06:34 -0300

    Adiciona o metodo PATCH como um metodo permitido

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -15,7 +15,7 @@ class App {
   private config(routes: IRoute[]):void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
       res.header('Access-Control-Allow-Headers', '*');
       next();
     };
