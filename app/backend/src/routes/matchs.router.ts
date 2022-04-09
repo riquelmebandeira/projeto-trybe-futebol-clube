7742a319db457110b5271ab28d069841294b8667
Riquelme <riquelmebandeira1@outlook.com>
2022-04-08 23:10:05 -0300

    Requisito 23 - (Router) implementa a rota POST /matchs

--- app/backend/src/routes/matchs.router.ts
+++ app/backend/src/routes/matchs.router.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from 'express';
+import validateJWT from '../middlewares';
 import { matchsControllerFactory } from '../factories';
 
 const matchsRouter = Router();
@@ -18,6 +19,24 @@ matchsRouter.get('/', async (req: Request, res: Response) => {
   const matchs = await matchsController.getAll();
 
   res.status(200).json(matchs);
+});
+
+matchsRouter.post('/', validateJWT, async (req: Request, res: Response) => {
+  const { homeTeam, awayTeam, inProgress } = req.body;
+
+  if (homeTeam === awayTeam) return res.status(400).json({ message: 'Clubs must be different' });
+
+  if (inProgress !== 'true') {
+    return res.status(400).json(
+      { message: 'The match must be in progress' },
+    );
+  }
+
+  const newMatch = await matchsController.createMatch(req.body);
+
+  if (!newMatch) return res.status(400).json({ message: 'Both clubs must exist' });
+
+  return res.status(201).json(newMatch);
 });
 
 export default matchsRouter;
