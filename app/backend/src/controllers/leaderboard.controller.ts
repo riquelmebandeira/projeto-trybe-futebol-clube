7291f163e121a4a925dc991407a69c634a6e7eba
Riquelme <riquelmebandeira1@outlook.com>
2022-04-11 10:59:08 -0300

    Requisito 31 - (Controller) Implementa a rota /leaderboard/away

--- app/backend/src/controllers/leaderboard.controller.ts
+++ app/backend/src/controllers/leaderboard.controller.ts
@@ -4,8 +4,8 @@ import ClubRanking from '../utils/ClubRanking';
 class LeaderboardController {
   constructor(readonly leaderboardService: LeaderboardService) {}
 
-  async getRanking(): Promise<ClubRanking[]> {
-    const result = await this.leaderboardService.getRankings();
+  async getRankingsOf(homeOrAwayClubs: string): Promise<ClubRanking[]> {
+    const result = await this.leaderboardService.getRankingsOf(homeOrAwayClubs);
 
     return result;
   }
