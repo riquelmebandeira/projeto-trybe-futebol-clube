66bd6d4bc4b016b52945c5a7c387a3be28b9fc8c
Riquelme <riquelmebandeira1@outlook.com>
2022-04-11 10:55:49 -0300

    Refatora a interface IMatch

--- app/backend/src/interfaces/match.interface.ts
+++ app/backend/src/interfaces/match.interface.ts
@@ -1,13 +1,13 @@
 interface IMatch {
   id?: number,
   homeTeam: number,
-  homeTeamGoals?: number,
+  homeTeamGoals: number,
   homeClub: {
     clubName: string,
     id: number,
   }
   awayTeam: number,
-  awayTeamGoals?: number,
+  awayTeamGoals: number,
   awayClub: {
     clubName: string,
     id: number,
