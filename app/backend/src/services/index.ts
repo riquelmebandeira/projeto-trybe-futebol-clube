6a8c6136dfffd59856a9536e13143176ed52d379
Riquelme <riquelmebandeira1@outlook.com>
2022-04-12 02:00:25 -0300

    Refatora o service de leaderboard

--- app/backend/src/services/index.ts
+++ app/backend/src/services/index.ts
@@ -1,6 +1,6 @@
 import LoginService from './login.service';
 import ClubsService from './clubs.service';
 import MatchsService from './matchs.service';
-import LeaderboardService from './leaderboards.service';
+import LeaderboardService from './leaderboard.service';
 
 export { LoginService, ClubsService, MatchsService, LeaderboardService };
---
+++ app/backend/src/services/leaderboard.service.ts
@@ -0,0 +1,60 @@
+import { IMatch, IRanking } from '../interfaces';
+import Club from '../database/models/Club';
+import Match from '../database/models/Match';
+import ClubRanking from '../utils/ClubRanking';
+import sortRankings from '../utils/sortRankings';
+
+class LeaderboardService {
+  readonly clubsModel = Club;
+
+  readonly matchsModel = Match;
+
+  private clubs: Club[];
+
+  private matchs: IMatch[];
+
+  constructor() {
+    this.getClubs();
+    this.getMatchs();
+  }
+
+  async getClubs(): Promise<void> {
+    this.clubs = await this.clubsModel.findAll();
+  }
+
+  async getMatchs(): Promise<void> {
+    this.matchs = await this.matchsModel.findAll({
+      where: {
+        inProgress: false,
+      },
+      include: [
+        { model: Club, as: 'homeClub' },
+        { model: Club, as: 'awayClub' },
+      ],
+    }) as unknown as IMatch[];
+  }
+
+  async getHomeRankings(): Promise<IRanking[]> {
+    const rankings = this.clubs.map(({ clubName, id }) => {
+      const clubRanking = new ClubRanking(clubName, id, this.matchs);
+
+      clubRanking.getHomeMatchsResults();
+
+      return clubRanking.showResults();
+    });
+    return sortRankings(rankings);
+  }
+
+  async getAwayRankings(): Promise<IRanking[]> {
+    const rankings = this.clubs.map(({ clubName, id }) => {
+      const clubRanking = new ClubRanking(clubName, id, this.matchs);
+
+      clubRanking.getAwayMatchsResults();
+
+      return clubRanking.showResults();
+    });
+    return sortRankings(rankings);
+  }
+}
+
+export default LeaderboardService;
