3e2112ca02e92797e868e9ed04856edf4af47a62
Riquelme <riquelmebandeira1@outlook.com>
2022-04-12 01:59:04 -0300

    Exporta a interface IRanking

--- app/backend/src/interfaces/index.ts
+++ app/backend/src/interfaces/index.ts
@@ -1,5 +1,6 @@
 import IRoute from './routes.interface';
 import IUser from './user.interface';
 import IMatch from './match.interface';
+import IRanking from './ranking.interface';
 
-export { IRoute, IUser, IMatch };
+export { IRoute, IUser, IMatch, IRanking };
